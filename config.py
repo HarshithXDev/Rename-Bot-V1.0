083e322e2285fe60f00c2c31d261906be1b5c995
HarshithXDev <harshithxdev@gmail.com>
2024-08-04 15:11:32 +0530

    Update config.py
    Signed-off-by: HarshithXDev <harshithxdev@gmail.com>

--- config.py
+++ config.py
@@ -2,17 +2,17 @@ import re, os
 
 id_pattern = re.compile(r'^.\d+$') 
 
-API_ID = os.environ.get("API_ID", "")
+API_ID = os.environ.get("API_ID", "27136615")
 
-API_HASH = os.environ.get("API_HASH", "")
+API_HASH = os.environ.get("API_HASH", "bb0b4dfb048fe0c002799e7c022b4f90")
 
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "") 
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "7071606442:AAEozO-Y9UPhehKrT3-QemqMLOPN_h_tdjY") 
 
-FORCE_SUB = os.environ.get("FORCE_SUB", "WebXBots") 
+FORCE_SUB = os.environ.get("FORCE_SUB", "HarshithXDev") 
 
-DB_NAME = os.environ.get("DB_NAME","")     
+DB_NAME = os.environ.get("DB_NAME","Cluster0")     
 
-DB_URL = os.environ.get("DB_URL","")
+DB_URL = os.environ.get("DB_URL","mongodb+srv://HarshithXDev:<HarshithXDev>@cluster0.pu2so1g.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
  
 FLOOD = int(os.environ.get("FLOOD", "10"))
 
